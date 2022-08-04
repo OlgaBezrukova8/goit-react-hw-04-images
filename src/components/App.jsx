1a80efe941c293d377d40e64ade891ddd300e6b2
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-04 18:02:01 +0100

    Added form submit and state to component App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,45 @@
-import { SearchBar } from './Searchbar/Searchbar';
+import { Component } from 'react';
+import * as API from '../services/searchImg-api.js';
 
-export const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+import { SearchBar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    images: [],
+    page: 1,
+    isLoading: false,
+    error: null,
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const currentValue = event.target.name.value;
+    const api = this.getImages(currentValue);
+    console.log(api);
+
+    this.setState(prevState => ({
+      images: [
+        ...prevState.images,
+        {
+          id: '',
+          webformatURL: '',
+          largeImageURL: '',
+        },
+      ],
+    }));
+  };
+
+  getImages = async name => {
+    await API.getImageAPI(name).then(response => console.log(response));
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmit={this.handleSubmit} />
+        <ImageGallery images={this.state.images} />
+      </div>
+    );
+  }
+}

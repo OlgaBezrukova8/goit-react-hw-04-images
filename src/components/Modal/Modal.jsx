4d559b0f694361fd912770ce684f51dc52cc6869
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-12 13:38:44 +0100

    Code refactoring: useEffect, useCallback hook in Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,36 @@
-import { Component } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, Window } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    image: PropTypes.string.isRequired,
-    onCloseModal: PropTypes.func.isRequired,
-  };
+export const Modal = ({ image, onCloseModal, onCloseModalEscape }) => {
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        onCloseModalEscape();
+      }
+    },
+    [onCloseModalEscape]
+  );
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [handleKeyDown]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  return createPortal(
+    <Overlay onClick={onCloseModal}>
+      <Window>
+        <img src={image} alt="" width="700" height="400" />
+      </Window>
+    </Overlay>,
+    modalRoot
+  );
+};
 
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onCloseModalEscape();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <Overlay onClick={this.props.onCloseModal}>
-        <Window>
-          <img src={this.props.image} alt="" width="700" height="400" />
-        </Window>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+Modal.propTypes = {
+  image: PropTypes.string.isRequired,
+  onCloseModal: PropTypes.func.isRequired,
+};

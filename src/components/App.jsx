64070b608d7b1386e2ac5c85d7cee747ab05978b
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-12 16:31:22 +0100

    Code refactoring: useState, useEffect, useCallback hook in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,37 +18,32 @@ export const App = () => {
   const [isEndReached, setIsEndReached] = useState(false);
   const [error, setError] = useState(null);
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { name, page } = this.state;
+  const fetchImages = useCallback(() => {
+    if (name.length === 0) {
+      return;
+    }
 
-  //   if (name !== prevState.name || page !== prevState.page) {
-  //     this.fetchImages();
-  //   }
-  // }
+    setIsLoading(true);
 
-  // fetchImages = async () => {
-  //   const { name, page } = this.state;
-  //   this.setState({ isLoading: true });
+    API.getImageAPI(name, page)
+      .then(response => {
+        setImages(prevState => [
+          ...prevState,
+          ...response.map(({ id, webformatURL, largeImageURL }) => {
+            return { id, webformatURL, largeImageURL };
+          }),
+        ]);
+        if (response.length < 12) {
+          setIsEndReached(true);
+        }
+      })
+      .catch(error => setError({ error }))
+      .finally(setIsLoading(false));
+  }, [name, page]);
 
-  //   try {
-  //     const response = await API.getImageAPI(name, page);
-  //     this.setState(prevState => ({
-  //       images: [
-  //         ...prevState.images,
-  //         ...response.map(({ id, webformatURL, largeImageURL }) => {
-  //           return { id, webformatURL, largeImageURL };
-  //         }),
-  //       ],
-  //     }));
-  //     if (response.length < 12) {
-  //       this.setState({ isEndReached: true });
-  //     }
-  //   } catch (error) {
-  //     this.setState({ error });
-  //   }
-
-  //   this.setState({ isLoading: false });
-  // };
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
 
   const handleSubmit = useCallback(
     event => {
@@ -73,23 +68,17 @@ export const App = () => {
     [images, name]
   );
 
-  const onCloseModalEscape = () => {
-    setImage('');
-  };
-
   const onCloseModal = event => {
     if (event.target === event.currentTarget) {
       setImage('');
     }
   };
 
-  const setCurrentImage = img => {
-    setImage(img);
-  };
+  const onCloseModalEscape = () => setImage('');
 
-  const counterPage = () => {
-    setPage(prevState => prevState + 1);
-  };
+  const setCurrentImage = img => setImage(img);
+
+  const counterPage = () => setPage(prevState => prevState + 1);
 
   return (
     <Container>
@@ -115,118 +104,3 @@ export const App = () => {
     </Container>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     images: [],
-//     name: '',
-//     image: '',
-//     page: 1,
-//     isLoading: false,
-//     isEndReached: false,
-//     error: null,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { name, page } = this.state;
-
-//     if (name !== prevState.name || page !== prevState.page) {
-//       this.fetchImages();
-//     }
-//   }
-
-//   fetchImages = async () => {
-//     const { name, page } = this.state;
-//     this.setState({ isLoading: true });
-
-//     try {
-//       const response = await API.getImageAPI(name, page);
-//       this.setState(prevState => ({
-//         images: [
-//           ...prevState.images,
-//           ...response.map(({ id, webformatURL, largeImageURL }) => {
-//             return { id, webformatURL, largeImageURL };
-//           }),
-//         ],
-//       }));
-//       if (response.length < 12) {
-//         this.setState({ isEndReached: true });
-//       }
-//     } catch (error) {
-//       this.setState({ error });
-//     }
-
-//     this.setState({ isLoading: false });
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-
-//     const { name, images } = this.state;
-//     const currentValue = event.target.name.value;
-
-//     if (currentValue === '' && images.length === 0) {
-//       Notify.warning('Whoops, incorrectly entered query');
-//     }
-
-//     if (currentValue === name) {
-//       Notify.info('Enter another query, please :)');
-//       return;
-//     }
-
-//     this.setState({
-//       name: currentValue,
-//       images: [],
-//       page: 1,
-//       isEndReached: false,
-//     });
-//   };
-
-//   onCloseModalEscape = () => {
-//     this.setState({ image: '' });
-//   };
-
-//   onCloseModal = event => {
-//     if (event.target === event.currentTarget) {
-//       this.setState({ image: '' });
-//     }
-//   };
-
-//   setCurrentImage = img => {
-//     this.setState({ image: img });
-//   };
-
-//   counterPage = () => {
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   };
-
-//   render() {
-//     const { images, image, isLoading, isEndReached, error } = this.state;
-//     return (
-//       <Container>
-//         {error &&
-//           Notify.failure(
-//             `Whoops, something went wrong: ${error.message}. Reload page, please`
-//           )}
-//         <SearchBar onSubmit={this.handleSubmit} />
-//         {images.length > 0 && (
-//           <ImageGallery
-//             images={images}
-//             setCurrentImage={this.setCurrentImage}
-//           />
-//         )}
-//         {image && (
-//           <Modal
-//             image={image}
-//             onCloseModalEscape={this.onCloseModalEscape}
-//             onCloseModal={this.onCloseModal}
-//           />
-//         )}
-//         {!isLoading && images.length > 0 && !isEndReached && (
-//           <Button title="Load more" showHandler={this.counterPage} />
-//         )}
-//         {isLoading && <Loader />}
-//       </Container>
-//     );
-//   }
-// }

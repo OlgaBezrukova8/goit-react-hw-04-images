0862841198071730dad92a6c834aabfc51c0da05
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-12 13:24:11 +0100

    Code refactoring

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,31 +1,27 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Header, Form, Button, ButtonLabel, Input } from './Searchbar.styled';
 
-export class SearchBar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+export const SearchBar = ({ onSubmit }) => {
 
-  render() {
-    const { onSubmit } = this.props;
+  return (
+    <Header>
+      <Form onSubmit={onSubmit}>
+        <Button type="submit">
+          <ButtonLabel>Search</ButtonLabel>
+        </Button>
 
-    return (
-      <Header>
-        <Form onSubmit={onSubmit}>
-          <Button type="submit">
-            <ButtonLabel>Search</ButtonLabel>
-          </Button>
+        <Input
+          type="text"
+          name="name"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </Form>
+    </Header>
+  );
+};
 
-          <Input
-            type="text"
-            name="name"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </Form>
-      </Header>
-    );
-  }
-}
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
